a042bea7a4e4778a8d7af6f52a8a9988bcde846e
antikanfous@gmail.com <antikanfous@gmail.com@f017c6a3-6b8f-7683-5ecb-adc374e41068>
2015-02-26 15:08:10 +0000

    pour amine

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,65 +9,57 @@ var direction = {
 var briqueX = 250,
     briqueY = 0;
 
-document.addEventListener("DOMContentLoaded", function (event) 
-{
+document.addEventListener("DOMContentLoaded", function (event) {
     canvas = document.getElementById('cnv1');
     ctx = canvas.getContext('2d');
     var heightCnv = canvas.height;
     var dir = null;
     var widthCnv = canvas.width;
     init();
-    
+
     // On teste les touches du clavier
-    document.onkeydown = function (e) 
-    {
-    	// 
-        switch (e.keyCode) 
-        {
-	        case 37:
-	            dir = direction.LEFT;
-	            animate();
-	            break;
-	        case 39:
-	            dir = direction.RIGHT;
-	            animate();
-	            break;
-	        case 40:
-	            dir = direction.DOWN;
-	            animate();
-	            break;
+    document.onkeydown = function (e) {
+        // 
+        switch (e.keyCode) {
+        case 37:
+            dir = direction.LEFT;
+            animate();
+            break;
+        case 39:
+            dir = direction.RIGHT;
+            animate();
+            break;
+        case 40:
+            dir = direction.DOWN;
+            animate();
+            break;
         }
     };
-    
+
     // On bouge la brique carré
-	function MoveSquare() {
-		
-            if (dir == direction.LEFT && briqueX > 100 && briqueY<450) 
-            {
-                briqueX -= 25;
-            } 
-            else if (dir == direction.RIGHT && briqueX < 400 && briqueY<450) 
-            {
-                briqueX += 25;
-            } 
-            else if (dir == direction.DOWN && briqueY < 425) // augmente la vitesse s'il tape down
-            { 
-                briqueY += 50;
-            }
-           
-            if (briqueY < 450) 
-        	{
-            	briqueY += 25;
-        	}
+    function MoveSquare() {
+
+        if (dir == direction.LEFT && briqueX > 100 && briqueY < 450) {
+            briqueX -= 50;
+        } else if (dir == direction.RIGHT && briqueX < 400 && briqueY < 450) {
+            briqueX += 50;
+        } else if (dir == direction.DOWN && briqueY < 425) // augmente la vitesse s'il tape down
+        {
+            briqueY += 50;
+        }
+
+        if (briqueY < 450) {
+            briqueY += 25;
+        }
         dir = null; // On annule la direction tant qu'on appuye sur aucune touche directionnelle
-	}
-	
-	// On dessine la brique carré
-    function drawSquare() {
-        ctx.strokeRect(briqueX, briqueY, 50, 50); 
     }
-	
-	// On anime la brique
+
+    // On dessine la brique carré
+    function drawSquare() {
+        ctx.strokeRect(briqueX, briqueY, 50, 50);
+    }
+
+    // On anime la brique
     function animate() {
         ctx.clearRect(100, 0, canvas.width - 100, canvas.height);
 
@@ -75,14 +67,14 @@ document.addEventListener("DOMContentLoaded", function (event)
         MoveSquare();
         drawSquare();
     }
-    
+
     // initialisation du canvas
     function init() {
-       	ctx.beginPath();
-    	ctx.moveTo(100, 0);
-    	ctx.lineTo(100, heightCnv);
-    	ctx.stroke();
-   		drawSquare();
+        ctx.beginPath();
+        ctx.moveTo(100, 0);
+        ctx.lineTo(100, heightCnv);
+        ctx.stroke();
+        drawSquare();
     }
 
     setInterval(animate, 200);
