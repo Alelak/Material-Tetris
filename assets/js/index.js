93ae362ea51c4835859ec95e2d18a2b28eaa156d
antikanfous@gmail.com <antikanfous@gmail.com@f017c6a3-6b8f-7683-5ecb-adc374e41068>
2015-03-01 03:40:00 +0000

    bug fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,17 +7,39 @@ var direction = {
     DOWN: 2,
     UP: 3
 };
-var briqueX = 250,
-    briqueY = 0;
+//var briqueX = 250,
+//    briqueY = 0;
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    "use strict";
     canvas = document.getElementById('cnv1');
     ctx = canvas.getContext('2d');
-    var heightCnv = canvas.height;
-    var widthCnv = canvas.width;
-    var test = new TetriminosO(briqueX, briqueY, ctx, direction, canvas);
-    var timer = new Timer();
-    init();
+    var heightCnv = canvas.height,
+        widthCnv = canvas.width,
+        tab = [],
+        i = 0,
+        dir = null;
+    for (i = 0; i < 10; i += 1) {
+        tab.push(new TetriminosO(ctx, direction, canvas));
+    }
+
+
+    document.onkeydown = function (e) {
+        switch (e.keyCode) {
+        case 37:
+            dir = direction.LEFT;
+            break;
+        case 39:
+            dir = direction.RIGHT;
+            break;
+        case 40:
+            dir = direction.DOWN;
+            break;
+        case 38:
+            dir = direction.UP;
+            break;
+        }
+    };
 
     function init() {
         ctx.beginPath();
@@ -25,10 +47,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
         ctx.lineTo(100, heightCnv);
         ctx.stroke();
     }
-    while (test.getY() < 450) {
-        if (timer.cooldown() % 200 == 0) {
-            test.animateSquare();
-        }
-    }
+    init();
+    var j = 0;
+    var arr = [];
+    setInterval(function () {
+        tab[j].animateSquare(dir);
 
+
+
+        if (j != tab.length && tab[j].posY == 450) {
+            arr.push(tab[j]);
+            j++;
+
+        }
+        for (var i = 0; i < arr.length; i++) {
+
+            arr[i].drawSquare();
+        }
+    }, 200)
 });
--- assets/js/tetriminosO.js
+++ assets/js/tetriminosO.js
@@ -1,53 +1,41 @@
-function TetriminosO(briqueX, briqueY, ctx, direction, canvas) {
+function TetriminosO(ctx, direction, canvas) {
     "use strict";
-    var pos = new Point(250, 0);
-    this.posX = pos.x;
-    this.posY = pos.y;
-    var dir = null;
+    // var pos = new Point(250, 0);
+    this.posX = 250;
+    this.posY = 0;
+    var self = this
 
-    document.onkeydown = function (e) {
-        switch (e.keyCode) {
-        case 37:
-            dir = direction.LEFT;
-            break;
-        case 39:
-            dir = direction.RIGHT;
-            break;
-        case 40:
-            dir = direction.DOWN;
-            break;
-        case 38:
-            dir = direction.UP;
-            break;
-        }
-    };
+
     this.setXY = function (x, y) {
         this.posX = x;
         this.posY = y;
-        console.log(x);
-        console.log(this.posY);
-    }
+    };
+
     this.getX = function () {
         return this.posX;
-    }
+    };
+
     this.getY = function () {
         return this.posY;
+    };
+    this.drawSquare = function () {
+        ctx.strokeRect(self.posX, self.posY, 50, 50);
     }
-    this.animateSquare = function () {
+
+    this.animateSquare = function (dir) {
         ctx.clearRect(100, 0, canvas.width - 100, canvas.height);
-        ctx.strokeRect(this.getX(), this.getY(), 50, 50);
-        if (dir == direction.LEFT && briqueX > 100 && briqueY < 450) {
-            briqueX -= 25;
-        } else if (dir == direction.RIGHT && briqueX < 400 && briqueY < 450) {
-            briqueX += 25;
-        } else if (dir == direction.DOWN && briqueY < 425) {
-            briqueY += 50;
+        self.drawSquare();
+        if (dir == direction.LEFT && self.posX > 100 && self.posY < 450) {
+            self.posX -= 25;
+        } else if (dir == direction.RIGHT && self.posX < 400 && self.posY < 450) {
+            self.posX += 25;
+        } else if (dir == direction.DOWN && self.posY < 425) {
+            self.posY += 50;
         }
 
-        if (briqueY < 450) {
-            briqueY += 25;
+        if (self.posY < 450) {
+            self.posY += 25;
         }
-        this.setXY(briqueX, briqueY);
-    }
+    };
 
 }
