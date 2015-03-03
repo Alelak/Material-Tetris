746c97fe1d023595919323c2bc130fc284092e47
antikanfous@gmail.com <antikanfous@gmail.com@f017c6a3-6b8f-7683-5ecb-adc374e41068>
2015-03-03 00:31:06 +0000

    version alpaha v1

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,18 +18,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
         widthCnv = canvas.width,
         piece = new TetriminosO(ctx, direction, canvas),
         dir = null;
-	var bool = false;
+    var bool = false;
     // on cree le tableau des lignes
-	var grid = new Array();
-	
-	// on cree les tableaux qui contiennent les colonnes de chaque ligne	
-	for(var i=0; i<20; i++)
-	   grid[i] = new Array();
-	
-	// On initialise chaque case de la grille à false puisque au début, elle est vide
-	for(var i=0; i<20; i++)
-	   for(var j=0; j<14; j++)
-	      grid[i][j] = false;
+    var grid = new Array();
+
+    // on cree les tableaux qui contiennent les colonnes de chaque ligne	
+    for (var i = 0; i < 20; i++)
+        grid[i] = new Array();
+
+    // On initialise chaque case de la grille à false puisque au début, elle est vide
+    for (var i = 0; i < 20; i++)
+        for (var j = 0; j < 14; j++)
+            grid[i][j] = false;
 
 
     document.onkeydown = function (e) {
@@ -57,44 +57,49 @@ document.addEventListener("DOMContentLoaded", function (event) {
         ctx.stroke();
        
     }*/
-    
-    //init();
-    
-    // Mettre les cases de la brique à true
-	for(var k=0; k<piece.squares.length;k++)
-	{
-		grid[piece.squares[k].i][piece.squares[k].j] = true;
-	}
-   
-    var myVar = setInterval(function () {
-		
-		piece.animateSquare(dir,grid);
-        // dans le cas ou la brique a était déposée, on annule le déplacement
-        
-        /*if (dir == direction.DOWN) {
-        	console.log("blabla");
-            clearInterval(myVar);
-        }*/
-       
-       if(piece.squares[3].i == 19)
-       {
-       		piece = new TetriminosO(ctx, direction, canvas);
-       		for(var k=0; k<piece.squares.length;k++)
-			{
-				grid[piece.squares[k].i][piece.squares[k].j] = true;
-			}
-       }
-       	
-        
-        //init();     
-        dir = null;
 
-    }, 200);
-    
-    
-    
+
+
+    //init();
+
+    // Mettre les cases de la brique à true
+    for (var k = 0; k < piece.squares.length; k++) {
+        grid[piece.squares[k].i][piece.squares[k].j] = true;
+    }
+
+    var myVar = setInterval(function () {
+
+            piece.animateSquare(dir, grid);
+            // dans le cas ou la brique a était déposée, on annule le déplacement
+
+            /*if (dir == direction.DOWN) {
+            	console.log("blabla");
+                clearInterval(myVar);
+            }*/
+            if (piece.stop) {
+                piece = new TetriminosO(ctx, direction, canvas);
+                for (var k = 0; k < piece.squares.length; k++) {
+                    grid[piece.squares[k].i][piece.squares[k].j] = true;
+                }
+                if (piece.isOver(grid)) {
+                    piece.drawSquare(); // pas bon !!!!!!!!!!!
+                    console.log('gameover');
+                    clearInterval(myVar);
+
+                }
+
+            }
+
+            dir = null;
+
+
+        },
+        200);
+
+
+
     /*if(piece.squares[3].i>17)
     			clearInterval(myVar);*/
-    
-    
+
+
 });
